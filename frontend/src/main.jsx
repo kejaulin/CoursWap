bb80e16105f41e116a4890424fb9b2961b635bd6
Oprah Nkam <oprah.j@Oprahs-Air.lan>
2025-05-29 12:27:01 +0200

    Ajout de l'intégration Google Calendar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
-import React from 'react-dom/client';
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-React.createRoot(document.getElementById('root')).render(
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="882025545288-d6jj90v0tlq2fm0rprshbvc1gfegvsvp.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
