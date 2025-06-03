db2aea0a9716f7544985c82418d05e176e126e5d
Oprah Nkam <oprah.j@wifi-etudiants832.utt.fr>
2025-06-03 10:19:51 +0200

    Ajout bdd/page dynamique

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.SERVER_PORT;
 
+
 mongoose.connect('mongodb://localhost/cours-wap-bdd').then(() => {
     console.log('Connected to MongoDB.');
   }).catch(error => {
@@ -23,3 +24,5 @@ app.get('/courswap', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}.`);
 }); 
+const professeursRoutes = require('./routes/professeur.js');
+app.use('/api/professeurs', professeursRoutes);
